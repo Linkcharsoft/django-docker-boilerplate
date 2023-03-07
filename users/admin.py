0717e60852f3e5ca99d9b4be0a02d420cb16ef8d
Juan Borrelli <nacho2911@hotmail.com>
2023-03-07 16:30:51 -0300

    minor changes

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
-from users.models import User_profile
+from users.models import UserProfile
 
 admin.site.register(User, UserAdmin)
 
-@admin.register(User_profile)
+@admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'birthdate']
 
